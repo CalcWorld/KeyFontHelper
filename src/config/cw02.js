0119a5c5393cd157288a4e51c0b61510273d6564
Calc.World <119807873+CalcWorld@users.noreply.github.com>
2025-05-24 19:06:32 +0800

    bug fix

--- src/config/cw02.js
+++ src/config/cw02.js
@@ -1,5 +1,18 @@
 import { cw01 } from "./cw01.js";
-import { copy, del, k_7_, k_arccos, k_arcsin, k_arctan, k_arctg, k_sen, k_tg, k_tg1, update } from "./_common.js";
+import {
+  copy,
+  del,
+  k_7_,
+  k_arccos,
+  k_arcsin,
+  k_arctan,
+  k_arctg,
+  k_rep,
+  k_sen,
+  k_tg,
+  k_tg1,
+  update
+} from "./_common.js";
 
 const cw02 = copy(cw01);
 update(cw02, {
@@ -7,6 +20,7 @@ update(cw02, {
   'f': ['[;]'],
   'r': ['(Arcsen)'],
   't': k_arccos,
+  'u': k_rep,
   'y': k_arctan,
   'D': ['(|__)', '(∟)', '(⌙)'],
   'G': ['(Rép)', '(Rep)'],
